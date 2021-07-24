69f6ec30bbe05b9765bca3482b4e944c647140d4
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-07-24 20:35:57 +1000

    Add solutions for string reversal problem

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,27 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    return str.split('').reduce((result, c) => c + result, '');
+}
 
 module.exports = reverse;
+
+// solution 2
+// function reverse(str) {
+//     var result = '';
+
+//     for (let c of str) {
+//         result = c + result;
+//     }
+
+//     return result;
+// }
+
+
+// solution 1
+// function reverse(str) {
+//     var charArray = str.split('');
+//     charArray = charArray.reverse();
+//     return charArray.join('');
+// }
