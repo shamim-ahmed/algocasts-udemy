d3b6412c430b013bfa7e34267470640ea7ffe6ea
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-07-25 21:07:46 +1000

    add solution to integer reversal problem

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,34 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    var result = 0;
+    var i = Math.abs(n);
+
+    while (i > 0) {
+        let r = i % 10;
+        i = Math.floor(i / 10);
+        result = 10 * result + r;
+    }
+
+    if (n < 0) {
+        result = -result;
+    }
+
+    return result;
+}
+
+// solution 1
+// function reverseInt(n) {
+//     var valueStr = Math.abs(n).toString();
+//     var resultStr = valueStr.split('').reverse().join('');
+//     var result = Number.parseInt(resultStr);
+
+//     if (n < 0) {
+//         result = -result;
+//     }
+
+//     return result;
+// }
 
 module.exports = reverseInt;
