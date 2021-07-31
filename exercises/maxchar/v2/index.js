383a5a74be71622cdea9eee034a8bc29afbb96b3
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-07-31 20:32:11 +1000

    add an alternative solution to maxchar problem

---
+++ exercises/maxchar/v2/index.js
@@ -0,0 +1,25 @@
+// --- Directions
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
+
+function maxChar(str) {
+    let data = {};
+    let maxChar = 0;
+    let maxValue = 0;
+
+    for (let c of str) {
+        data[c] = data[c] + 1 || 1;
+
+        if (data[c] > maxValue) {
+            maxValue = data[c];
+            maxChar = c;
+        }
+    }
+
+    return maxChar;
+}
+
+module.exports = maxChar;
