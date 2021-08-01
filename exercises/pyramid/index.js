de4643150249e594c6da212ea0ba7ca18720cea6
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-08-01 21:27:21 +1000

    add solution to pyramid printing problem

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,25 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+
+    for (let i = 1; i <= n; i++) {
+        let str = '';
+
+        for (let j = 1; j <= n - i; j++) {
+            str += ' ';
+        }
+
+        for (let j = 1; j <= 2 * i - 1; j++) {
+            str += '#';
+        }
+
+        for (let j = 1; j <= n - i; j++) {
+            str += ' ';
+        }
+
+        console.log(str);
+    }
+}
 
 module.exports = pyramid;
