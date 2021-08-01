91ee09ee54f2a46f8fb2ab75f6c8869163421779
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-08-01 13:42:57 +1000

    add an alternate solution to array chunk problem

---
+++ exercises/chunk/v3/index.js
@@ -0,0 +1,24 @@
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+function chunk(array, size) {
+    const resultArray = [];
+    let index = 0;
+
+    while (index < array.length) {
+        let data = array.slice(index, index + size);
+        resultArray.push(data);
+        index += size;
+    }
+
+    return resultArray;
+}
+
+module.exports = chunk;
