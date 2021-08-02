477ad336ed377b401b43d225f00f1f2030d4ab4e
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-08-02 23:23:25 +1000

    add solution to vowel counting problem

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,17 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    let input = str.toLowerCase();
+    let n = 0;
+
+    for (let c of input) {
+        if (c === 'a' || c === 'e' || c === 'i' || c === 'o' || c === 'u') {
+            n++;
+        }
+    }
+
+    return n;
+}
 
 module.exports = vowels;
