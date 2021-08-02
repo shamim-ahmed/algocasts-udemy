086f86888f6bf96370f9e3850abf88de5796d13f
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-08-02 23:41:05 +1000

    add an alternative solution using regular expression

---
+++ exercises/vowels/v3/index.js
@@ -0,0 +1,16 @@
+// --- Directions
+// Write a function that returns the number of vowels
+// used in a string.  Vowels are the characters 'a', 'e'
+// 'i', 'o', and 'u'.
+// --- Examples
+//   vowels('Hi There!') --> 3
+//   vowels('Why do you ask?') --> 4
+//   vowels('Why?') --> 0
+
+function vowels(str) {
+    const matches = str.match(/[aeiou]/gi);
+
+    return !matches ? 0 : matches.length;
+}
+
+module.exports = vowels;
