424d0e91acbc745142f88ae95c4ed5a90624ae14
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-08-05 00:36:07 +1000

    add a simple queue implementation

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -8,6 +8,18 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+class Queue {
+    constructor() {
+        this.values = [];
+    }
+
+    add(n) {
+        this.values.unshift(n);
+    }
+
+    remove() {
+        return this.values.pop();
+    }
+}
 
 module.exports = Queue;
