3cf5e6d9e2f71509dd19bc9f048f268869ede0cb
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-08-08 16:49:04 +1000

    add solution to 'From Last' problem

--- exercises/fromlast/index.js
+++ exercises/fromlast/index.js
@@ -11,6 +11,22 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+function fromLast(list, n) {
+    let slow = list.getFirst();
+    let fast = list.getFirst();
+    let step = n;
+
+    while (step > 0) {
+        fast = fast.next;
+        step--;
+    }
+
+    while (fast.next != null) {
+        slow = slow.next;
+        fast = fast.next;
+    }
+
+    return slow;
+}
 
 module.exports = fromLast;
