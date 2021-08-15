dc5b39c88aa73a26f13530ff8d9049e79a7f6b1d
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-08-15 16:59:26 +1000

    implement 'insert' and 'contains' method for a BST node

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,42 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+    constructor(data, left = null, right = null) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+
+    insert(key) {
+        if (key < this.data) {
+            if (this.left != null) {
+                this.left.insert(key);
+            } else {
+                this.left = new Node(key);
+            }
+        } else if (key > this.data) {
+            if (this.right != null) {
+                this.right.insert(key);
+            } else {
+                this.right = new Node(key);
+            }
+        }
+    }
+
+    contains(key) {
+        if (key === this.data) {
+            return this;
+        }
+
+        if (key < this.data) {
+            return this.left != null ? this.left.contains(key) : null;
+        } 
+
+        if (key > this.data) {
+            return this.right != null ? this.right.contains(key) : null;
+        }
+    }
+}
 
 module.exports = Node;
