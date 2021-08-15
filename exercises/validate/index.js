b6bae9806f1eeb84bfc541a706d9ccb1a9d6c38a
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-08-15 20:44:49 +1000

    add solution to bst validation problem

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,23 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+function validate(node, min = null, max = null) {
+
+    if (min !== null && node.data < min) {
+        return false;
+    } else if (max !== null && node.data > max) {
+        return false;
+    }
+
+    if (node.left !== null && validate(node.left, min, node.data) === false) {
+        return false;
+    }
+
+    if (node.right !== null && validate(node.right, node.data, max) === false) {
+        return false;
+    }
+
+    return true;
+}
 
 module.exports = validate;
