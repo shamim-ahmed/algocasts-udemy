63112800fd7a849282077dcabf4109c011a4105e
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-08-15 14:15:06 +1000

    add solution to levelwidth problem

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,27 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    if (root === null) {
+        return []
+    }
+
+    let nodeArray = [root];
+    const lengthArray = [];
+    
+    while (nodeArray.length > 0) {
+        lengthArray.push(nodeArray.length);
+        const childArray = [];
+
+        for (let i = 0; i < nodeArray.length; i++) {
+            const node = nodeArray[i];
+            childArray.push(...node.children);
+        }
+
+        nodeArray = childArray;
+    }
+
+    return lengthArray;
+}
 
 module.exports = levelWidth;
