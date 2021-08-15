f6d6d6e6ba6871d535fdca004dd38ac6b94f5b5f
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-08-15 21:07:08 +1000

    implement a solution for event handling problem

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,43 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  constructor() {
+    this.eventHandlerMap = {};
+  }
+
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    let handlerArray = this.eventHandlerMap[eventName];
+
+    if (!handlerArray) {
+      handlerArray = [];
+      this.eventHandlerMap[eventName] = handlerArray;
+    }
+
+    handlerArray.push(callback);
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    const handlerArray = this.eventHandlerMap[eventName];
+
+    if (handlerArray) {
+      for (let callback of handlerArray) {
+        callback();
+      }
+    }
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    const handlerArray = this.eventHandlerMap[eventName];
+
+    if (handlerArray) {
+      handlerArray.splice(0, handlerArray.length);
+    }
+  }
 }
 
 module.exports = Events;
