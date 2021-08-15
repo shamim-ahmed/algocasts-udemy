c135a757d92077d3af3a2126b2b1c90e9b31eb21
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-08-15 22:57:52 +1000

    add solution to various sorting related problems

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,19 +2,83 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                const tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
 
+    return arr;
 }
 
 function selectionSort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        let minValue = arr[i];
+        let minIndex = i;
 
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < minValue) {
+                minValue = arr[j];
+                minIndex = j;
+            }
+        }
+
+        if (minIndex !== i) {
+            const tmp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = tmp;
+        }
+    }
+
+    return arr;
 }
 
 function mergeSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
 
+    const mid = Math.floor(arr.length / 2);
+    let left = arr.slice(0, mid);
+    let right = arr.slice(mid, arr.length);
+    
+    left = mergeSort(left);
+    right = mergeSort(right);
+
+    const result = merge(left, right);
+    return result;
 }
 
 function merge(left, right) {
+    const result = [];
+    let i = 0;
+    let j = 0;
 
+    while (i < left.length && j < right.length) {
+        if (left[i] < right[j]) {
+            result.push(left[i]);
+            i++;
+        } else {
+            result.push(right[j]);
+            j++;
+        }
+    }
+
+    while (i < left.length) {
+        result.push(left[i]);
+        i++;
+    }
+
+    while (j < right.length) {
+        result.push(right[j]);
+        j++;
+    }
+
+    return result;
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
